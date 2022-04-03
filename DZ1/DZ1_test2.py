6a846c2861a269e7f6254774f3b1eaf7c412579e
a13261326 <a13261326@gmail.com>
2022-04-03 07:34:02 +0300

    убрал токен

--- DZ1/DZ1_test2.py
+++ DZ1/DZ1_test2.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import json
 
 user_id = '143181516'
-access_token = '7f39788b56e80b8a3b5f394081aec4fa877ae4560c23cf916f1d3f1ac2e52ab3b2de338a81480c46720f0'
+access_token = ''
 url = f'https://api.vk.com/method/groups.get?user_ids={user_id}&fields=bdate&extended=1&access_token={access_token}&v=5.131'
 
 
